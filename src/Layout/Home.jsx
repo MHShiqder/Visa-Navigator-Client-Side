3e9537ba09c1e88bf93c22b461d7b9ae6565ae71
Hasibul Hasan <hasibul277127@gmail.com>
2024-12-08 17:21:24 +0600

    added the theme button

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -2,11 +2,17 @@ import LatestVisa from "../Component/LatestVisa";
 import Newsletter from "../Component/Newsletter";
 import Slideshow from "../Component/Slideshow";
 import Success from "../Component/Success";
+import { useEffect } from 'react'
+import { themeChange } from 'theme-change'
 
 
 const Home = () => {
+    useEffect(() => {
+        themeChange(false)
+      }, [])
     return (
-        <div>
+        <div className="text-center">
+            <button data-toggle-theme="dark,light" data-act-class="ACTIVECLASS" className="btn btn-outline mt-5 ">Dark \ Light</button>
             <Slideshow></Slideshow>
             <LatestVisa></LatestVisa>
             <Newsletter></Newsletter>
